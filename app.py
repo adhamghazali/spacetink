3e0c740824b104d604827124a5f8d2b123701ee5
Adham <adham.ghazali@gmail.com>
2022-08-02 17:30:10 +0300

    some progress

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-"""
-# My first app
-Here's our first attempt at using data to create a table:
-"""
 
 import streamlit as st
-
 
 st.title("Automatic Character Creation")
 st.markdown('choose type of characeter')
 
 
 option = st.selectbox("",('Human', 'Robot', 'Vehicle' , 'Animal'))
+
+text=st.text_input('describe your Character: ')
+
+clicked=st.button('Generate 3D model')
+
+if clicked: 
+    video_file = open('./images/donkey_final_crop.mp4', 'rb')
+    data = video_file.read()
+    st.video(data, format="video/mp4", start_time=0)
 
